a135e82c1c0ddace34dba76279cd66529c67f004
jcmsbits <josecarlos.nago.machado50@gmail.com>
2025-01-23 04:22:38 -0500

    else y finally

--- zero_division_error.py
+++ zero_division_error.py
@@ -7,10 +7,28 @@
 # En Try ponemos el código que puede dar error
 # Y en Except lo que queremos que se ejecute si da el error
 
-num_1 = 10
-num_2 = 0
+try:
+    num_1 = 10
+    num_2 = 0
+    num_3 = 3
 
-result = num_1 / num_2
+    # result = num_1 / num_2
+    result = num_1 / num_3
 
-print("El resultado de la operacion es:", result)
-print("El programa ha finalizado.")
+    print("El resultado de la operacion es:", result)
+    print("El programa ha finalizado.")
+
+# Si queremos para errores específico escribimos:
+# except Tipo_de_Error_a_manejar
+except ZeroDivisionError as error:
+    print("Lo sentimos, no fue posible completar la operación.")
+    print(error)
+
+# También podemos usar else para ejecutar algo sino hubo problemas
+else: 
+    print("El resultado de la operación es: ", result)
+
+# Y si queremos ejecutar código independientemente si hubo error o no
+# Usamos finally
+finally:
+    print("El programa ha finalizado")

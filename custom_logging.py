e49a6e3843967e0ce6422d6675d6c62dd536ea94
jcmsbits <josecarlos.nago.machado50@gmail.com>
2025-01-24 15:09:44 -0500

    Comentando el traceback

--- custom_logging.py
+++ custom_logging.py
@@ -13,6 +13,8 @@ try:
 except Exception as error:
     exception_info = {
         "message" : str(error),
+        # traceback por defecto se activa solo sino usamos try y except
+        # entonces tenemos usar format_exc
         "detail" : traceback.format_exc()
     }
     logging.error(exception_info)
